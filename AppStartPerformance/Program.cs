fa17e370acf167627db0bd90102b6b93ee8410d6
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2020-11-05 16:32:02 +0300

    Add project files.

---
+++ AppStartPerformance/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AppStartPerformance {
+    class Program {
+        const int attempts = 10;
+        static void Main(string[] args) {
+            MeasureStartTime("XtraSpreadsheet w/o ribbon", @"..\..\..\WinSpreadsheetNoRibbon\bin\Release\WinSpreadsheetNoRibbon.exe", attempts);
+            MeasureStartTime("XtraSpreadsheet ribbon runtime", @"..\..\..\WinSpreadsheetRibbonRuntime\bin\Release\WinSpreadsheetRibbonRuntime.exe", attempts);
+            MeasureStartTime("XtraSpreadsheet ribbon designtime", @"..\..\..\WinSpreadsheetRibbonDesigntime\bin\Release\WinSpreadsheetRibbonDesigntime.exe", attempts);
+            Console.WriteLine("Done! Press any key to continue...");
+            Console.ReadLine();
+        }
+
+        static void MeasureStartTime(string name, string appPath, int n) {
+            if (n < 1)
+                throw new ArgumentException("Are you dumb?");
+            Console.Write("Running {0}:", name);
+            var times = new double[n];
+            for (int i = 0; i < n; i++) {
+                Console.Write(" {0}", i + 1);
+                var time = MeasureOneRun(appPath);
+                times[i] = time.TotalMilliseconds;
+            }
+            Console.WriteLine();
+            var result = PrepareResult(name, times);
+            PrintResult(result);
+        }
+
+        static TimeSpan MeasureOneRun(string appPath) {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var process = Process.Start(appPath);
+            process.WaitForExit(10000);
+            sw.Stop();
+            return sw.Elapsed;
+        }
+
+        static PerfResult PrepareResult(string name, double[] times) {
+            Array.Sort(times);
+            int n = times.Length;
+            var result = new PerfResult() { 
+                Name = name,
+                Min = times[0],
+                Max = times[n - 1]
+            };
+            double total = 0;
+            for (int i = 0; i < n; i++)
+                total += times[i];
+            result.Mean = total / n;
+            if (n == 1)
+                result.Median = times[0];
+            else if (n % 2 == 1)
+                result.Median = times[n / 2];
+            else
+                result.Median = (times[n / 2] + times[n / 2 - 1]) / 2;
+            return result;
+        }
+
+        static void PrintResult(PerfResult result) {
+            Console.WriteLine(result.Name);
+            Console.WriteLine("Min:    {0} ms", result.Min);
+            Console.WriteLine("Max:    {0} ms", result.Max);
+            Console.WriteLine("Avg:    {0} ms", result.Mean);
+            Console.WriteLine("Median: {0} ms", result.Median);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ WinSpreadsheetRibbonRuntime/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSpreadsheetRibbonRuntime {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            var ribbon = spreadsheetControl1.CreateRibbon();
+            ribbon.Parent = this;
+            ribbon.Dock = DockStyle.Top;
+            var sb = spreadsheetControl1.CreateRibbonStatusBar(ribbon);
+            sb.Parent = this;
+            sb.Dock = DockStyle.Bottom;
+            spreadsheetControl1.Dock = DockStyle.Fill;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e) {
+            Close();
+        }
+    }
+}
